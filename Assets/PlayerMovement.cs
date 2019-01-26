e77382a61a0df7053197fc4dc09edbc911bb8dcd
Mike Pors <mike.pors@gmail.com>
2019-01-26 16:18:19 -0700

    Updates PlayerMovement to work on 3d elements

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,38 +4,27 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 5.0f;
-	public float snap=1.0f;
 
-	private Rigidbody2D self;
+	private Rigidbody self;
 	private float inputX;
 	private float inputY;
 	private Vector2 cumulativeMovement;
 
-	// Use this for initialization
 	void Start () {
-		self = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		self = GetComponent<Rigidbody> ();
 	}
 
-	// Update is called at fixed intervals
+	void Update () {
+
+	}
+
 	void FixedUpdate(){
-		// * speed * Time.deltaTime
 		self.MovePosition (
 			self.position + ( speed * Time.deltaTime * getInput () )
 		);
-		//Debug.Log ( self.position );
 	}
 
-	// Returns input normalized to the unit circle
-	private Vector2 getInput(){	
-		return (new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"))).normalized;
-	}
-
-	private Vector2 snapToPixels(Vector2 before){
-		return (new Vector2( snap * Mathf.Round(before.x / snap), snap * Mathf.Round(before.y / snap)));
+	private Vector3 getInput(){
+		return (new Vector3 (Input.GetAxisRaw ("Horizontal"),0, Input.GetAxisRaw ("Vertical"))).normalized;
 	}
 }
