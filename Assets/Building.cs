4e5e42d1f235cbbcf82af7a55d58669034f3d603
elijahlucian <elijahlucian@gmail.com>
2019-01-26 14:28:16 -0700

    added start and end positions

--- Assets/Building.cs
+++ Assets/Building.cs
@@ -6,22 +6,31 @@ public class Building : MonoBehaviour
 {
 
     int size = 20;
+    
     Transform[] points;
     public Transform buildingObject;
+    public GameObject roads;
 
     // Start is called before the first frame update
     void Start()
     {
+        float offset = size / 2;    
+        // points = new Transform[size];
         
-        points = new Transform[size];
-        
-        for (int i = 0; i < size; i += 2)
-        {
-            Transform point = Instantiate(buildingObject);
-            point.localScale = new Vector3(1,1,1);
-            point.SetParent(transform, false);
-            points[i] = point;
-        }
+        // use roads game object
+        // get roads.children
+        // for each road 
+            // for length 
+
+        // for (int i = 0; i < size; i += 1) {
+            
+            // float height = Random.Range(1.0f,2.0f);
+            // Transform building = Instantiate(buildingObject);
+            // building.localScale = new Vector3(0.5f,height,0.5f);
+            // building.localPosition = new Vector3(i - offset, height, 0);
+            // building.SetParent(transform, false);
+            // points[i] = building;
+        // }
     }
 
     // Update is called once per frame
---
+++ Assets/World.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+	int grid_u = 20;
+	int grid_v = 20;
+	private Texture2D noiseTex;
+	[Range(1,4)] public int road_max;
+	Transform[] worldGrid;
+	public Transform treeObject;
+	
+	// Use this for initialization
+	void Start () {
+
+		// starting point
+		// ending point
+		// two roads
+		// birds
+		// park
+		// buildings
+		// houses
+
+		worldGrid = new Transform[grid_u * grid_v];
+
+		for(int i = 0; i < road_max; i++) {
+			// make roads
+		}
+
+		for(int u = 0; u < grid_u; u++) {
+			for(int v = 0; v < grid_v; v++) {
+				float rnd = Random.Range(0, 10);
+				Debug.Log(rnd);
+				if( rnd < 1 ) {
+					
+					Transform tree = Instantiate(treeObject);
+					tree.localScale = new Vector3(0.5f,1,0.5f);
+					tree.localPosition = new Vector3(u - grid_u/2,1,v - grid_v/2);
+					tree.SetParent(transform, false);
+					worldGrid[u] = tree;
+				}
+			}
+		}
+	}
+	
+	void GenerateTree () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
