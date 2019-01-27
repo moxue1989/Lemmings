8638d444590794cc4c0e94a27e6879f32d5d50a6
elijahlucian <elijahlucian@gmail.com>
2019-01-26 17:53:04 -0700

    added some super wacky birds

---
+++ Assets/BlueBird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBird : MonoBehaviour {
+
+	Transform bird;
+	float offset;
+	// Use this for initialization
+	void Start () {
+		bird = GetComponent<Transform>();
+		offset = Random.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+		Vector3 position = bird.position;
+		position.y += Mathf.Sin(Time.time * offset * Mathf.PI * 2 ) / 20;
+		bird.localPosition = position;
+	}
+}
--- Assets/World.cs
+++ Assets/World.cs
@@ -8,10 +8,17 @@ public class World : MonoBehaviour {
 	int grid_v = 20;
 	private Texture2D noiseTex;
 	[Range(1,4)] public int road_max;
+	[Range(2,10)] public int bird_count;
 	public Transform[,] worldGrid;
+	public int[,] navGrid;
 	public Transform treeObject;
 	public Transform roadObject;
-	
+	public Transform blueBirdObject;
+	public Transform yellowBirdObject;
+	public Transform Birds;
+
+	public Transform Roads;
+	public Transform Trees;
 	int road_u;
 	int road_v;
 
@@ -23,7 +30,7 @@ public class World : MonoBehaviour {
 		// park
 		// buildings
 		// houses
-
+		
 		worldGrid = new Transform[grid_u, grid_v];
 
 		for(int i = 0; i < road_max; i++) {
@@ -37,16 +44,20 @@ public class World : MonoBehaviour {
 			for(int v = 0; v < grid_v; v++) {
 				
 				if (worldGrid[u,v] == null) {
-				
 					float rnd = Random.value;
 					if( rnd < 0.02 ) {
-						Transform tree = Instantiate(treeObject);
-						tree.localPosition = new Vector3(u - grid_u/2,1,v - grid_v/2);
-						tree.SetParent(transform, false);
-						worldGrid[u,v] = tree;
+						worldGrid[u,v] = GenerateTree(u,v);
 					}
 				}
 			}
+		}
+
+		for(int i = 0; i < bird_count; i++) {
+			bool is_blueBird = Random.value > 0.5f;
+			Transform bird = Instantiate(is_blueBird ? blueBirdObject : yellowBirdObject);
+			bird.localPosition = new Vector3(Random.Range(-8,8),2,Random.Range(-8,8));
+			bird.SetParent(Birds, false);
+
 		}
 
 		// call navmesh builder
@@ -58,7 +69,7 @@ public class World : MonoBehaviour {
 		for (int k = 0; k < (is_u ? grid_u : grid_v) ; k++) {
 			
 			Transform road = Instantiate(roadObject);
-			road.SetParent(transform, false);
+			road.SetParent(Roads, false);
 			road.localPosition = is_u ? new Vector3(road_coordinate,0,k - 10) : new Vector3(k - 10,0,road_coordinate);
 			if(is_u) {
 				road.Rotate(0,90,0);
@@ -69,8 +80,11 @@ public class World : MonoBehaviour {
 			
 		}
 	}	
-	void GenerateTree () {
-
+	Transform GenerateTree (int u, int v) {
+		Transform tree = Instantiate(treeObject);
+		tree.localPosition = new Vector3(u - grid_u/2,1,v - grid_v/2);
+		tree.SetParent(Trees, false);
+		return tree;
 	}
 
 	// Update is called once per frame
