639290ce66dae89d5af6ddea5dc8cf9af24ca344
elijahlucian <elijahlucian@gmail.com>
2019-01-27 12:28:03 -0700

    updating gitignore, we need meta

--- Assets/Seedling.cs
+++ Assets/Seedling.cs
@@ -1,16 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
 
 	public Rigidbody rb;
-<<<<<<< HEAD
     protected internal int force;
     public GameObject timer;
-=======
->>>>>>> 9d23f757f0201639df70b0a503c578020a7305fe
 
 	// Use this for initialization
 	void Start () {
