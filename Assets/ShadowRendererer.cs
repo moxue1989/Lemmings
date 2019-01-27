c37118a83bab374748fd4df9cfd4b2e863f07e31
elijahlucian <elijahlucian@gmail.com>
2019-01-27 12:51:00 -0700

    player is the player, Moe

---
+++ Assets/ShadowRendererer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowRendererer : MonoBehaviour {
+	Renderer renderer;
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer>();
+		renderer.receiveShadows = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
